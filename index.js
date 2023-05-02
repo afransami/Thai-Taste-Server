76a79727bd3ce80854b4abc91ed640d6d2ac1126
afransami <afran.sami007@gmail.com>
2023-05-02 15:46:37 +0600

    json update and card added

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-
 const port = process.env.PORT || 5000;
-
-const data = require ('./data/data.json');
-
+const recipeData = require ('./data/data.json');
 
 app.use(cors());
+
 app.get ('/', (req, res)=>{
-    res.send ('Chef cooking')
+    res.send('chef is cooking')
 });
 
 app.get ('/data', (req, res)=>{
-res.send (data);
+res.send (recipeData);
 })
 
 
+
 app.listen(port, ()=>{
-    console.log(`Dragon API is running on port: ${port}`);
+    console.log(`chef API is running on port: ${port}`);
 })
