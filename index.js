a0602608b53deff3e53648555b4efe9d1c86364a
afransami <afran.sami007@gmail.com>
2023-05-02 10:29:40 +0600

    route setup

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 const express = require('express')
 const app = express()
-
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
 const data = require ('./data/data.json');
 
 
-
+app.use(cors());
 app.get ('/', (req, res)=>{
-    res.send ('Dragon is running')
+    res.send ('Chef cooking')
 });
 
 app.get ('/data', (req, res)=>{
 res.send (data);
-})
-
-
-app.get ('/data/:id', (req, res)=>{
-    const id = parseInt(req.params.id);
-    console.log(id);   
-    if(id === 0 ){
-        res.send (data.data)
-    } else{
-        const newdata = data.filter(n=> parseInt(n.id) ===id)    
-        res.send(newdata);
-    }    
 })
 
 
