9593d4fb0e8759cc8db3fa956e8cd7f312b9bd66
afransami <afran.sami007@gmail.com>
2023-05-03 00:19:42 +0600

    google and github signing

--- index.js
+++ index.js
@@ -4,15 +4,36 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const recipeData = require ('./data/data.json');
 
+
 app.use(cors());
 
-app.get ('/', (req, res)=>{
-    res.send('chef is cooking')
-});
+// app.get ('/', (req, res)=>{
+//     res.send('chef is cooking')
+// });
 
 app.get ('/data', (req, res)=>{
 res.send (recipeData);
 })
+
+
+app.get('id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    const selectedData = recipeData.find(n=> n.id === id);
+    res.send(selectedData);
+})
+
+
+
+// app.get ('/data/:id', (req, res)=>{
+//     const id = parseInt(req.params.id);
+//     console.log(id);   
+//     if(id === 0 ){
+//         res.send (recipeData)
+//     } else{
+//         const getData = recipeData.filter(r=> parseInt(r.id) ===id)    
+//         res.send(getData);        
+//     }    
+// })
 
 
 
