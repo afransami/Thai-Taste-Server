407f5d132945a5e992892636877254a62196febb
afransami <afran.sami007@gmail.com>
2023-05-03 16:30:03 +0600

    single data found

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const recipeData = require ('./data/data.json');
 const singleRecipe = require ('./data/singleRecipe.json');
+const data = require ('./data/data.json');
 
 
 
@@ -13,34 +13,44 @@ app.get ('/', (req, res)=>{
     res.send('chef is cooking')
 });
 
-app.get ('/recipeData', (req, res)=>{
-res.send (recipeData);
-})
-
 app.get ('/singleRecipe', (req, res)=>{
 res.send (singleRecipe);
 })
 
+app.get ('/data', (req, res)=>{
+res.send (data);
+})
 
-app.get('/singleRecipe/:id', (req, res)=>{
+
+app.get('/data/:id', (req, res)=>{
     const id = req.params.id;    
-    const selectedData = singleRecipe.find(n=> n.id === id);
+    const selectedData = data.find(n=> n.id === id);
     res.send(selectedData);
 })
 
 
 
-app.get ('/recipeData/:id', (req, res)=>{
+// app.get ('/data/:id', (req, res)=>{
+//     const id = parseInt(req.params.id);
+//     console.log(id);   
+//     if(id === 0 ){
+//         res.send (data)
+//     } else{
+//         const getData = data.filter(r=> parseInt(r.id) === id)    
+//         res.send(getData);        
+//     }    
+// })
+
+app.get ('/singleRecipe/:id', (req, res)=>{
     const id = parseInt(req.params.id);
     console.log(id);   
     if(id === 0 ){
-        res.send (recipeData)
+        res.send (data)
     } else{
-        const getData = recipeData.filter(r=> parseInt(r.id) === id)    
+        const getData = data.filter(r=> parseInt(r.id) === id)    
         res.send(getData);        
     }    
 })
-
 
 
 app.listen(port, ()=>{
